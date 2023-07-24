9fb4309c220242221e1593a51248a30991b50a8f
Mazahir02 <mazahir.nazmi@gmail.com>
2023-07-24 15:48:46 +0530

    Create changeDatasourcePassword4.py

---
+++ changeDatasourcePassword4.py
@@ -0,0 +1,37 @@
+# The encrypted password for the datasources
+DBPASSWORD = '{AES256}Dn04cfUTAurjzqFi4i4xJCIb811sLXyCEamvOierBD0='
+
+# Specify the path of the WebLogic domain
+DOMAIN_PATH = '/u02/oracle/Oracle/Middleware/Oracle_Home/domains/fmw_domain'
+
+# List of datasource names for which the password will be updated
+DS_NAME = ['myDataSource', 'myDataSource1']
+
+# Connect to the WebLogic domain
+connect()
+
+# Start editing the domain configuration
+edit()
+startEdit()
+
+# Navigate to the JDBCSystemResources to get all datasources
+cd('JDBCSystemResources')
+allDS = cmo.getJDBCSystemResources()
+
+# Loop through all datasources and find the specified DS_NAMEs
+for tmpDS in allDS:
+    for dsName in DS_NAME:
+        if dsName == tmpDS.getName():
+            # If DS_NAME matches, change the password for the datasource
+            print 'Changing the Password for DataSource ', dsName
+            cd('/JDBCSystemResources/' + dsName + '/JDBCResource/' + dsName + '/JDBCDriverParams/' + dsName)
+            set('PasswordEncrypted', DBPASSWORD)
+
+# Save the changes made to the configuration
+save()
+
+# Activate the changes to make them effective
+activate()
+
+# Disconnect from the WebLogic domain
+disconnect()
